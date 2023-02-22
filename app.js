6810c3a85f371ccf2ec9905f6b4c397f314f98fb
Raka Agi Saputra <Raka_Agi_Saputra@Teknokrat.ac.id>
2023-02-23 06:03:19 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// const http = require('http');
+
+// const server = http.createServer();
+// const port = 5000;
+// const host = 'localhost'
+// server.listen(port,host,()=>{
+//     console.log(`server berjalan pada http://${host}:${port}`);
+// })
